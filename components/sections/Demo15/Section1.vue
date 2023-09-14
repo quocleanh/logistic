1566d6071af4967456d55ae6f110d6be4e9194b4
Quốc Lê <quoc.leanh.001@gmail.com>
2023-09-14 20:41:44 +0700

    qáta

--- components/sections/Demo15/Section1.vue
+++ components/sections/Demo15/Section1.vue
@@ -6,19 +6,19 @@
                 <div class="col-md-6">
                     <div class="hero-15-txt wow fadeInRight">
                         <!-- Title -->
-                        <h2 class="s-64 w-700">Marketing strategies for a digital life</h2>
+                        <h2 class="s-64 w-700">Nền tảng quản lý vận chuyển hàng đầu</h2>
                         <!-- Text -->
-                        <p class="p-lg">Mauris donec turpis suscipit sapien ociis sagittis sapien tempor a volute ligula and aliquet tortor</p>
+                        <p class="p-lg"> Dễ dàng quản lý vận đơn, doanh thu và bảng giá theo từng tệp khách hàng với BookShip.vn. Với sự tiện lợi và linh hoạt, chúng tôi giúp bạn tổ chức, theo dõi và tối ưu hóa quy trình vận chuyển một cách hiệu quả.</p>
                         <!-- Button -->
-                        <a href="#banner-7" class="btn r-04 btn--theme hover--tra-black">Get started for free</a>
-                        <p class="p-sm btn-txt ico-15"><span class="flaticon-check"></span> No credit card needed, free 14-day trial</p>
+                        <a href="#banner-7" class="btn r-04 btn--theme hover--tra-black">Dùng thử miễn phí</a>
+                        <p class="p-sm btn-txt ico-15"><span class="flaticon-check"></span> 3 tháng dùng thử, lên đến 10k vận đơn. Hủy bất kì lúc nào</p>
                     </div>
                 </div>
                 <!-- END HERO TEXT -->
                 <!-- HERO IMAGE -->
                 <div class="col-md-6">
                     <div class="hero-15-img text-center wow fadeInLeft">
-                        <img class="img-fluid" src="/assets/images/hero-15-img.png" alt="hero-image" />
+                        <img class="img-fluid" src="/assets/images/home/hero-banner-7.png" alt="hero-image" />
                     </div>
                 </div>
             </div>
--- components/sections/Demo3/Section1.vue
+++ components/sections/Demo3/Section1.vue
@@ -6,9 +6,9 @@
                 <div class="col-md-6">
                     <div class="hero-3-txt color--white wow fadeInRight">
                         <!-- Title -->
-                        <h2 class="s-45 w-700">Swift Reservations, Lower Costs: ShipGate Leads the Way!</h2>
+                        <h2 class="s-45 w-700">Nền tảng quản lý vận đơn hàng đầu </h2>
                         <!-- Text -->
-                        <p class="p-lg">ShipGate revolutionizes logistics with lightning-fast booking, securing unbeatable rates. Simplify your shipping process and save time and money, all in one powerful platform</p>
+                        <p class="p-lg"> Dễ dàng quản lý vận đơn, doanh thu và bảng giá theo từng tệp khách hàng với ShipGate. Với sự tiện lợi và linh hoạt, chúng tôi giúp bạn tổ chức, theo dõi và tối ưu hóa quy trình vận chuyển một cách hiệu quả</p>
                         <!-- HERO DIGITS -->
                         <div class="hero-digits">
                             <!-- DIGIT BLOCK #1 -->
@@ -16,11 +16,11 @@
                                 <div class="hero-digits-block">
                                     <!-- Digit -->
                                     <div class="block-digit">
-                                        <h2 class="s-46 statistic-number">3<span>x</span></h2>
+                                        <h2 class="s-46 statistic-number">3<span>tháng</span></h2>
                                     </div>
                                     <!-- Text -->
                                     <div class="block-txt">
-                                        <p class="p-sm">Tempor sapien and quaerat placerat</p>
+                                        <p class="p-sm">Dùng thử <br>miễn phí</p>
                                     </div>
                                 </div>
                             </div>
@@ -30,11 +30,11 @@
                                 <div class="hero-digits-block">
                                     <!-- Digit -->
                                     <div class="block-digit">
-                                        <h2 class="s-46 statistic-number">29<span>%</span></h2>
+                                        <h2 class="s-46 statistic-number">-50<span>%</span></h2>
                                     </div>
                                     <!-- Text -->
                                     <div class="block-txt">
-                                        <p class="p-sm">Ligula suscipit vitae and rutrum turpis</p>
+                                        <p class="p-sm">Chi phí <br>vận hành</p>
                                     </div>
                                 </div>
                             </div>
@@ -47,7 +47,7 @@
                 <!-- HERO IMAGE -->
                 <div class="col-md-6">
                     <div class="hero-3-img wow fadeInLeft">
-                        <img class="img-fluid" src="/assets/images/tablet-01.png" alt="hero-image" />
+                        <img class="img-fluid" src="/assets/images/home/hero-banner-8.png" alt="hero-image" />
                     </div>
                 </div>
             </div>
